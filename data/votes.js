b5e722eaf46afcb90e344e86f6d8a43155efd786
jd richards <jdrichardstech@gmail.com>
2022-07-28 13:34:53 -0400

    change vote data, add arguments

--- data/votes.js
+++ data/votes.js
@@ -1,10 +1,13 @@
 const votes = [
-  { name: 'john', vote: 'yes' },
-  { name: 'lois', vote: 'no' },
-  { name: 'pam', vote: 'yes' },
-  { name: 'pavlova', vote: 'no' },
-  { name: 'paloma', vote: 'yes' },
-  { name: 'olga', vote: 'undecided' }
+  { name: 'john', vote: 'Pink Elephants' },
+  { name: 'lois', vote: 'Kyona' },
+  { name: 'pam', vote: 'Pink Elephants' },
+  { name: 'pavlova', vote: 'In the Middle of Nowhere' },
+  { name: 'paloma', vote: 'Pink Elephants' },
+  { name: 'olga', vote: 'undecided' },
+  { name: 'cherise', vote: 'undecided' },
+  { name: 'parker', vote: 'Samui Sunrise' },
+  { name: 'eloise', vote: 'Samui Sunrise' }
 ];
 
 module.exports = votes;
